5b21a2c246b5173fb678363bcc61cfe1b4c38dd8
jiajin <670626858@qq.com>
2021-10-09 22:15:39 +0800

    update gitalk id

--- models/GitalkModels.js
+++ models/GitalkModels.js
@@ -1,12 +1,12 @@
 const GithubAttributes = {
       /** Github repo url */
-      repo: "jiaj-blog",
+      repo: "jiaj-blog-web",
       /** Github username */
       owner: "OneTake1998",
       /** Github app client id */
-      clientID: "951ba883629b61c9fa33",
+      clientID: "e7948dd5120f7ef846e1",
       /** Github app client secret */
-      clientSecret: "6287318d154b2a9d29cd245053c2e2fffd7f2dda",
+      clientSecret: "3cce8ad45e750ece336ce0cede542b53cdbf5d43",
       /** Github app admin username */
       admin: "OneTake1998"
 }
