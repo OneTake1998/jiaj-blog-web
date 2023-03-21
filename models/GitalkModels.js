c8871cf1d9b928a4ba788e6bb1b2d433980d85f3
jiajin <670626858@qq.com>
2023-03-21 20:04:04 +0800

    修改

--- models/GitalkModels.js
+++ models/GitalkModels.js
@@ -4,9 +4,9 @@ const GithubAttributes = {
       /** Github username */
       owner: "OneTake1998",
       /** Github app client id */
-      clientID: "输入你的clientID",
+      clientID: "e7948dd5120f7ef846e1",
       /** Github app client secret */
-      clientSecret: "输入你的clientSecret",
+      clientSecret: "1f6533ad41a52fa952fb56707249021a91278697",
       /** Github app admin username */
       admin: "OneTake1998"
 }
